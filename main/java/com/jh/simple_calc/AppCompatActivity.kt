e3b5e244a0cf82fe50ed589bed5f9363136810c8
Jeonghun HAN <101242920+Hisabori@users.noreply.github.com>
2023-07-09 12:12:15 +0900

    CalcTest_#2 계산기 TEST 버전_계발중 코드 오류 수정 중 dev/first commit 2023-07-08 17:15 #1 commit 2023-07-09 12:11 #2

--- main/java/com/jh/simple_calc/AppCompatActivity.kt
+++ main/java/com/jh/simple_calc/AppCompatActivity.kt
@@ -1,5 +1,11 @@
 package com.jh.simple_calc
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+
 open class AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
+    open fun onCreate(savedInstanceState: Bundle?) {
+    }
 }
--- main/java/com/jh/simple_calc/MainActivity.kt
+++ main/java/com/jh/simple_calc/MainActivity.kt
@@ -1,52 +1,59 @@
 package com.jh.simple_calc
 
-
+import android.annotation.SuppressLint
 import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var editText: TextView
+
+    private lateinit var editText: EditText
     private lateinit var button: Button
     private lateinit var textView: TextView
     private val input = StringBuilder()
-    private var num1 = 0.0
-    private var num2 = 0.0
+    private var num1: Double = 0.0
+    private var num2: Double = 0.0
     private var operator: String? = null
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         editText = findViewById(R.id.editText)
         button = findViewById(R.id.button)
-        textView = findViewById(R.id.TextView)
+        textView = findViewById(R.id.textView)
+
+        button.setOnClickListener {
+            val btn = it as Button
+            val btnText = btn.text.toString()
+
+            when (btnText) {
+                "c" -> input.setLength(0)
+                "=" -> {
+                    num2 = input.toString().toDouble()
+                    input.setLength(0)
+                    calculate()
+                    operator = null
+                }
+                "+", "-", "*", "/" -> {
+                    num1 = input.toString().toDouble()
+                    input.setLength(0)
+                    operator = btnText
+                }
+                else -> input.append(btnText)
+            }
+
+            textView.text = input.toString()
+        }
     }
 
-
-    fun onButtonClick(v: View) {
-        val btn = v as Button
-        val btnText = btn.text.toString()
-
-        when (btnText) {
-            "c" -> input.setLength(0)
-            "=" -> {
-                num2 = input.toString().toDouble()
-                input.setLength(0)
-                calculate()
-                operator = null
-            }
-            "+", "-", "*", "/" -> {
-                num1 = input.toString().toDouble()
-                input.setLength(0)
-                operator = btnText
-            }
-            else -> input.append(btnText)
-        }
-
-        textView.text = input.toString()
+    private fun setContentView(activityMain: Any) {
+        TODO("Not yet implemented")
     }
 
     private fun calculate() {
@@ -61,8 +68,4 @@ class MainActivity : AppCompatActivity() {
 
         input.append(result)
     }
-}
-
-private fun Any.onCreate(savedInstanceState: Bundle?) {
-    TODO("Not yet implemented")
 }
